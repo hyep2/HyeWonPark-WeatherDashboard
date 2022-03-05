04ac96688f47556d1fa0850fe3119d3233114134
Hye Won Park <hyewonpark26@gmail.com>
2022-03-04 22:57:03 -0800

    adds readme, last changes

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# HyeWonPark-WeatherDashboard
+# WEATHER DASHBOARD
+```
+The function of this app is to provide the user with the current weather, along with the weather for the next five days. The weather information included consists of the following: temperature, wind speed, humidity, and UV index. There will also be a weather icon for each day. The UV index is color-coded by severity. The user can search for the city he wants to check the weather in and the app will backlog the history of his searches, creating buttons for each city's input. When he clicks on those buttons, the weather information for those city will display again. 
+
+
+```
+## ALGORITHM 
+```
+
+1. Set up a basic HTML and CSS page to begin the project
+2. Set up basic JS to begin the project
+3. Connect APIs 
+4. Build Carousel funcionality
+5. Build search bar funcionality w/ API
+```
+
+## LINK TO DEPLOYED APP 
+
+![app picture](./weatherpic.png)
+
--- script.js
+++ script.js
@@ -1,15 +1,43 @@
 //need to add if statement for cityinput =null;
 
+let cityName = "";
 document.getElementById('submitBtn').addEventListener("click", event => {
   event.preventDefault();
-  
 
-  let cityName = document.getElementById('cityInput').value;
+  //refreshing every time you submit a new city
+  refresh();
+  
+  cityName = document.getElementById('cityInput').value;
+
+  //jic user puts nothing in search form
+  if (cityName == "") {
+    document.getElementById('today').style.visibility = "hidden";
+    alert('Need to put city name');
+    return;
+  }
+
+  displayWeather();
   localStorage.setItem('city', cityName);
   let history = document.getElementById('buttonRef');
   let historyBtn = document.createElement('button');
-  historyBtn.innerHTML= cityName;
+  historyBtn.innerHTML = cityName;
   history.append(historyBtn);
+  historyBtn.addEventListener('click', event => {
+    event.preventDefault();
+    cityName = historyBtn.innerHTML;
+    refresh();
+    displayWeather();
+  })
+
+
+
+}
+)
+
+
+//this fxn is for fetching all the data and displaying all the data output
+function displayWeather() {
+
 
   let apiKey = 'ae5d20128353d0f3fbf8bec684d4d5c1'
 
@@ -24,7 +52,6 @@ document.getElementById('submitBtn').addEventListener("click", event => {
     .then(function (data) {
       let lon = data.city.coord['lon']
       let lat = data.city.coord['lat']
-      console.log(data);
       //current weather api url
       let currentUrl = 'https://api.openweathermap.org/data/2.5/weather?lat=' + lat + '&lon=' + lon + '&appid=' + apiKey;
 
@@ -37,13 +64,12 @@ document.getElementById('submitBtn').addEventListener("click", event => {
           return res.json();
         })
         .then(five_data => {
-          console.log(five_data);
           //for current day
           let curr_temp = 1.8 * (five_data.current.temp - 273) + 32;
           let currRound = Math.round((curr_temp + Number.EPSILON) * 100) / 100
           let icon = five_data.current.weather[0].icon;
           let curr_humidity = five_data.current.humidity;
-          let curr_wind = five_data.current.weather.wind_speed;
+          let curr_wind = five_data.current.wind_speed;
           let curr_uv = five_data.current.uvi;
           //adding uvi badge
           let uviBadge = document.createElement('button');
@@ -74,7 +100,7 @@ document.getElementById('submitBtn').addEventListener("click", event => {
           document.getElementById("currentUV").textContent = "UV index: ";
           document.getElementById('currentUV').append(uviBadge);
 
-          document.getElementById('today').style.visibility ="visible";
+          document.getElementById('today').style.visibility = "visible";
 
           //getting days 1-5 after current day 0
           for (let i = 1; i < 6; i++) {
@@ -86,7 +112,7 @@ document.getElementById('submitBtn').addEventListener("click", event => {
 
             let dayDiv = document.createElement('div');
             let iconDiv = document.createElement('div');
-            
+
             //need to add header too with the dates and image
 
             let tomorrow = moment().add(i, 'days').format('MM/DD/YYYY');
@@ -98,7 +124,7 @@ document.getElementById('submitBtn').addEventListener("click", event => {
             let windEl = document.createElement('p');
             let humidityEl = document.createElement('p');
             let iconEl = document.createElement('img')
-            
+
             //icon for the five day weather
             let iconsrc = five_data.daily[i].weather[0].icon;
             let fivesource = "http://openweathermap.org/img/wn/" + iconsrc + "@4x.png"
@@ -126,7 +152,7 @@ document.getElementById('submitBtn').addEventListener("click", event => {
             bigDay.append(iconDiv);
 
             mainDiv.append(bigDay);
-      
+
 
           }
         })
@@ -135,16 +161,15 @@ document.getElementById('submitBtn').addEventListener("click", event => {
 
     })
 }
-)
 
+//this fxn is for refreshing every time there is a new city input
 function refresh() {
-  let x=document.getElementById("week");
-  if(x.childElementCount!=0) {
-    x.removeChild();
-  }
-  else {
-    return;
-  }
+  removeChilds(document.getElementById("week"));
+  removeChilds(document.getElementById('todayIcon'));
 }
 
-
+const removeChilds = (parent) => {
+  while (parent.lastChild) {
+    parent.removeChild(parent.lastChild);
+  }
+};
